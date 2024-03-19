cd34d07c7ec4ade04297a91a840de35c888f9fa1
rosieyxl <rosieyxl@gmail.com>
2024-03-19 10:21:55 -0400

    code for random assigning random graphics to each card (paired up)

--- VGA.c
+++ VGA.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
 
 int main(void) {
-  // code here
+    srand(time(0)); // seed with time
+    int graphics[8] = {1,2,3,4,1,2,3,4}; // the possible graphics
+    int cards[8] = {}; // the array of cards; will hold their corresponding graphic
+    int j;
+
+    for (int i = 0; i < 8; i++){
+        // assign a graphic to each card
+        j = rand()%(8-i);
+        printf((char)j);
+        cards[i] = graphics[j];
+        // remove the value we just used & shift the rest of the items down
+        if (j != 7) {
+            for (int k = j; k < (8-i); k++) {
+                graphics[k] = graphics[k+1];
+            }
+        }
+    }
+
+    // debugging
+    printf("This is the final array: \n");
+    for (int i = 0; i <8; i++){
+        printf((char)cards[i]);
+    }
 }
